bda82009a872ae8b8a8222515c8e233422ac73bc
David Slochower <slochower@gmail.com>
2019-08-13 20:53:40 -0700

    Every atom gets a unique atom type

--- speakeasy/smirnoff.py
+++ speakeasy/smirnoff.py
@@ -85,6 +85,7 @@ def write_smirnoff_frcmod(conversion, topology, system, with_fixes=True):
     # FIXME: OpenForceField does not provide atom names!
     for index, atom in enumerate(structure.atoms, 1):
         atom.name = f"{atom.element_name}{np.random.randint(0, 1000)}"
+        atom.type = f"{atom.type}{np.random.randint(0, 10)}"
 
     structure.save(smirnoff_prmtop, format="amber")
     structure.save(smirnoff_mol2)
